ecaa0899a86c54f675cdbacfc524fe0dbbe3355b
Robert - Andrei Miron <mrabobi@users.noreply.github.com>
2019-04-22 01:00:49 +0300

    Upload iteratia2
    Cod + testing

---
+++ docs/modul-algdist/Iteratia2/proiectIpit2/junit.testing/DateBd/ComisieLicentaTest.java
@@ -0,0 +1,17 @@
+package DateBd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComisieLicentaTest {
+
+    @Test
+    void getSala() {
+
+    }
+
+    @Test
+    void getIdComisie() {
+    }
+}
