f9ebc14422c0190f1a68683c3eaa55eca51ed925
Robert - Andrei Miron <mrabobi@users.noreply.github.com>
2019-04-22 13:25:59 +0300

    Update testing

--- docs/modul-algdist/Iteratia2/proiectIpit2/junit.testing/DateBd/ComisieLicentaTest.java
+++ docs/modul-algdist/Iteratia2/proiectIpit2/junit.testing/DateBd/ComisieLicentaTest.java
@@ -6,12 +6,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ComisieLicentaTest {
 
-    @Test
-    void getSala() {
-
-    }
 
     @Test
     void getIdComisie() {
+        ComisieLicenta comisie = new ComisieLicenta(1,2,3,4);
+        assertEquals(1,comisie.getIdProfPresedinte());
     }
 }
